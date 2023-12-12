365c5872a0d243635068e5124a3d055513c56f8b
ashwindesai5 <39927596+ashwindesai5@users.noreply.github.com>
2023-12-11 16:19:28 -0800

    Update app.R

--- app.R
+++ app.R
@@ -1,13 +1,20 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
-library(fmsb)
-library(stringr)
 library(plotly)
 
 source("finalproject.R")
+
 diabetes_df <- read.csv("diabetes.csv") 
 wa_df <- read.csv("wa_df.csv")
+
+state_chioce <- unique(diabetes_df$LocationDesc)
+
+race_choice <- c("Hispanic", "Black, non-Hispanic", "American Indian or Alaska Native", 
+                  "White, non-Hispanic", "Asian or Pacific Islander")
+
+#gender_choice <- unique(diabetes_df$Stratification1)
+gender_choice <- c("Male", "Female")
 
 
 introduction_view <- fluidPage(
@@ -19,37 +26,18 @@ introduction_view <- fluidPage(
     practices and contribute to efforts aimed at reducing diabetes rates.")
 )
 
-analysis_view<-fluidPage(
-  titlePanel("Diabetes Analysis"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(
-        inputId = "state_name",
-        label = "Select a state",
-        choices = diabetes_df$LocationDesc #filtered dataframe of choosen state
-      )
-    ),
-    mainPanel(
-      tabsetPanel(
-        tabPanel("VS WA", h3("Selected State Compared to Washington"),plotlyOutput(outputId = "plot1")),
-        tabPanel("Gender", h3("Comparison of Selected States Diabetic Outcomes in terms of Gender"),plotlyOutput(outputId = "plot2")),
-        tabPanel("Race", h3("Comparison of Selected States Diabetic Outcomes in terms of Gender"),plotlyOutput(outputId = "plot3"))
-      )
-    )
-  ),
-)
-
 summary_view <- fluidPage(
   h1("Summary"),
   p("From the backgroud study, we obtian that individuals' daily sugar intake 
     values are siginificantly higher than the FDA’s recommendation(less than 10%). 
     This is a significant issue for people to draw attentions on. Our study compares 
     people with different races, gender and regions focused in westcost on the 
-    number of diagnosed diabetes cases. We found out that people in Washington 
-    state have higher diagnosed rate than people who lives in California. 
-    Hispanics have higher rate than overall. Based on the anaylsis, we hypothises 
+    number of diagnosed diabetes cases. People living in southeastern part of the 
+    US tends to get higher rate of diabete diagnosis. Asisna and pacific lslander
+    have higher rate than overall. Based on the anaylsis, we hypothises 
     that the reason for these result could be culturally dietary pattern difference 
     caused by factors of races, genders and regions."),
+  br(),
   p("Data sources:"),
   p("CDC's Division of Population Health (U.S. Chronic Disease Indicators):"),
   p("124 indicators designed for uniform collection and reporting of chronic disease data.
@@ -61,6 +49,87 @@ summary_view <- fluidPage(
   Facilitating the analysis of dietary patterns to understand their correlation with diabetes rates.")
 )
 
+analysis_view<-fluidPage(
+  tabsetPanel(
+    tabPanel("VS WA", 
+             
+             sidebarLayout(
+               sidebarPanel(
+                 h2("Control panel"),
+                 selectInput(
+                   inputId = "state_name",
+                   label = "Select a state",
+                   choices = state_chioce
+                 )
+               ),
+               mainPanel(
+                 h3("Selected State Compared to Washington"), 
+                 plotlyOutput(outputId = "plot1")
+               )
+             ),
+             p("The bar graph offers a comparative analysis between the number of 
+               individuals with diabetes in Washington and those from a user-specified state. 
+               The x-axis delineates the years under consideration, while the y-axis reflects 
+               the respective counts of people hospitalized with diabetes as a listed cause. 
+               It's important to note that there may be variations in data availability between 
+               the states, and there is a possibility of missing data for certain years. 
+               The graph facilitates a dynamic comparison, allowing users to select a 
+               specific state for juxtaposition with Washington. This interactive feature 
+               empowers users to discern patterns and disparities in diabetes prevalence 
+               between the two chosen states across different years, contributing to a 
+               comprehensive understanding of regional health trends.")
+
+    ),
+    tabPanel("Gender", 
+             h3("Comparison of Selected States Diabetic Outcomes in terms of Gender"), 
+             plotlyOutput(outputId = "plot2"),
+             p("The bar graph provides a comparative analysis of the annual count 
+               of individuals with diabetes, categorized by gender, specifically 
+               focusing on the selected state as specified by the user from provies
+               plot. The x-axis represents the chronological progression of years, 
+               while the y-axis depicts the corresponding number of people hospitalized 
+               with diabetes as a listed cause. It is essential to acknowledge the 
+               possibility of missing data for certain years or genders within the chosen state. 
+               In some instances, certain years may not have available data for a 
+               particular gender. The graph's interactive functionality allows users 
+               to tailor their exploration by selecting a specific state, enabling a 
+               more nuanced examination of diabetes prevalence across different 
+               genders and years within the chosen state.")
+             
+    ),
+    tabPanel("Race", 
+             sidebarLayout(
+               sidebarPanel(
+                 h2("Control panel"),
+                 selectInput(
+                   inputId = "race",
+                   label = "Select a race",
+                   choices = race_choice
+                 )
+               ),
+               mainPanel(
+                 h3("Comparison of Selected States Diabetic Outcomes in terms of Race"), 
+                 plotlyOutput(outputId = "plot3")
+               )
+             ),
+             p("The bar graph illustrates the yearly count of individuals with diabetes, 
+               specifically those identifying as [input race from dropdown, who 
+               were hospitalized with diabetes as a listed cause. The x-axis 
+               corresponds to the year, while the y-axis denotes the number of 
+               people affected. It is important to note that there might be instances 
+               of missing data for certain states, and it is possible that some 
+               states may not have any available data. The graph features interactivity, 
+               enabling users to choose a specific race from a dropdown menu. 
+               Upon selection, the graph dynamically updates to display the corresponding 
+               data for people with diabetes from the chosen race for each year. 
+               This interactive feature facilitates easy comparison of diabetes 
+               prevalence across various races and years.")
+
+    )
+  )
+  
+)
+
 ui<-navbarPage(
   "Final Project",
   tabPanel("Introduction", introduction_view),
@@ -68,52 +137,60 @@ ui<-navbarPage(
   tabPanel("Summary", summary_view)
 )
 
-server<-function(input, output){
-  
-  filtered_state <- function(state){
-    #filtered table goes here
-    selected_data <- filter(diabetes_df, LocationDesc == "state_name")
-  }
- 
-
+server <- function(input, output){
   output$plot1 <- renderPlotly({
-    #plot goes here
-  merged <- full_join(filtered_state, wa_df)
-   a <- ggplot(merged, aes(YearStart, DataValue))+
-    geom_col(aes(color = YearStart)) +
-    labs( x = "Year",
-         y = "Total Population with Diabetes") +
-    facet_wrap(~LocationDesc)
+    selected_state <- filter(diabetes_df, LocationDesc == input$state_name, Stratification1 == "Overall", 
+                             Question == "Hospitalization with diabetes as a listed diagnosis")
+    selected_state <- filter(selected_state, !is.na(DataValue))
+    cleaned_wa <- filter(wa_df, Stratification1 == "Overall", 
+                         Question == "Hospitalization with diabetes as a listed diagnosis")
+    cleaned_wa <- filter(cleaned_wa, !is.na(DataValue))
     
-    return (a)
+    merged <- full_join(selected_state, cleaned_wa)
+    a <- ggplot(merged, aes(YearStart, DataValue, fill = LocationDesc)) +
+      geom_col() +
+      scale_y_continuous(labels = scales::comma_format(scale = 1e-3, suffix = "k"),
+                         breaks = scales::pretty_breaks(5)) +
+      labs(x = "Year",
+           y = "Total Population with Diabetes (in thousands)",
+           fill = "Legend") +
+      facet_wrap(~LocationDesc)
+    ggplotly(a)
   })
+  
+  output$plot2 <- renderPlotly({
+    filtered_data <- filter(diabetes_df, LocationDesc == input$state_name, StratificationCategory1 == "Gender")
+    b <- ggplot(filtered_data, aes(x = YearStart, y = DataValue, fill = Stratification1)) +
+      geom_bar(stat = "identity", position = "dodge") +
+      labs(title = paste("Number of People with Diabetes by Gender in", input$state_name),
+           x = "Year", y = "Number of People with Diabetes",
+           fill = "Gender") +
+      theme_minimal() +
+      theme(legend.position = "top")
+    
+    ggplotly(b)
+  })
+  
+  output$plot3 <- renderPlotly({
+    filtered_data <- reactive({
+      race_df <- wa_df[wa_df$DataValueType == "Number" & !is.na(wa_df$YearStart), ]
+      race_df$DataValue <- as.numeric(as.character(race_df$DataValue))
+      race_df <- race_df[race_df$DataValueType == "Number" & race_df$StratificationCategory1 == "Race/Ethnicity" & race_df$Stratification1 == input$race, ]
+      race_df
+    })
+    
+    c <- ggplot(filtered_data(), aes(x = YearStart, y = DataValue)) +
+      geom_col() +
+      scale_y_continuous(labels = scales::comma_format(scale = 1e-3),
+                         breaks = scales::pretty_breaks(5)) +
+      labs(x = "Year", y = "Number of People With Diabetes (Thousands)")
+    ggplotly(c)
+  })
+
+  
+  
+  
 }
-
-output$plot2 <- renderPlotly({
-    #plot goes here
-    gender_df <- filter(filtered_state, StratificationCategory1 == "Gender")
-  b <- ggplot(gender_df, aes(YearStart, DataValue, group = StratificationCategory1))+
-    geom_col(aes(color = StratificationCategory1)) +
-    labs( x = "Year",
-         y = "Total Population with Diabetes") +
-    facet_wrap(~StratificationCategory1)
-    
-    return (b)
-  })
- 
-
-output$plot3 <- renderPlotly({
-    #plot goes here
-    race_df <- filter(filtered_state, StratificationCategory1 == "Race/Ethnicity")
-  c <- ggplot(gender_df, aes(YearStart, DataValue, group = StratificationCategory1))+
-    geom_col(aes(color = StratificationCategory1)) +
-    labs( x = "Year",
-         y = "Total Population with Diabetes") +
-    facet_wrap(~StratificationCategory1)
-    
-    return (c)
-  })
-
 
 
 shinyApp(ui=ui,server=server)
